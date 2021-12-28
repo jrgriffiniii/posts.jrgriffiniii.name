5a02ec1f4acdc43618a66b20b8e60d86f47f51e1
jrgriffiniii <jrgriffiniii.k0idl@simplelogin.co>
2021-12-28 01:38:56 -0500

    Integrating support for Sass and reimplementing CSS into SCSS templates

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -3,8 +3,9 @@ import "typeface-montserrat"
 import "typeface-merriweather"
 // normalize CSS across browsers
 import "./src/normalize.css"
-// custom CSS styles
-import "./src/style.css"
+
+// Custom SCSS templates
+import "./src/scss/app.scss"
 
 // Highlighting for code blocks
 import "prismjs/themes/prism.css"
